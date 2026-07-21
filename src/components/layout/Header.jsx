f5c312a8271fa6dd0ae830bfe897c8fcca150ed5
quanghoatrinh <quanghoa.trinh@gmail.com>
2026-07-21 20:23:30 +0700

    demo modal

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -1,7 +1,15 @@
-import { Button, Container, Nav, Navbar, NavDropdown } from "react-bootstrap";
+import { useState } from "react";
+import { Button, Container, Form, Modal, Nav, Navbar, NavDropdown } from "react-bootstrap";
 
 function Header(){
-    return (
+  const [show, setShow] = useState(false);
+
+  const handleClose = ()=>{
+    setShow(false);
+  };
+  const handleShow = () => setShow(true);
+
+  return (
        <Navbar expand="lg" className="bg-body-tertiary">
       <Container>
         <Navbar.Brand href="#home">T2605E</Navbar.Brand>
@@ -21,10 +29,41 @@ function Header(){
                 Separated link
               </NavDropdown.Item>
             </NavDropdown>
-            <Nav.Link>Login</Nav.Link>
+            <Nav.Link onClick={handleShow}>Login</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Login</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Form>
+            <Form.Group className="mb-3" controlId="formBasicEmail">
+              <Form.Label>Email address</Form.Label>
+              <Form.Control type="email" placeholder="Enter email" required />
+              <Form.Text className="text-muted">
+                We'll never share your email with anyone else.
+              </Form.Text>
+            </Form.Group>
+
+            <Form.Group className="mb-3" controlId="formBasicPassword">
+              <Form.Label>Password</Form.Label>
+              <Form.Control type="password" placeholder="Password" />
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="formBasicCheckbox">
+              <Form.Check type="checkbox" label="Check me out" />
+            </Form.Group>
+            <Button variant="primary" type="submit">
+              Submit
+            </Button>
+          </Form>
+        </Modal.Body>
+        <Modal.Footer>
+        
+      
+        </Modal.Footer>
+      </Modal>
     </Navbar>
     )
 }
